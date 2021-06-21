7ce81facd0613079aff323360498ec765d04c51b
amir_za <amirz@traxretail.com>
2021-06-21 18:08:05 +0300

    test(server): :white_check_mark: setup test environemnt + e2e tests

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,13 +12,63 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await app.close();
+    1;
+  });
+
+  it('/roll - with missing frame error', async () => {
+    await request(app.getHttpServer())
+      .post('/roll')
+      .send({ rollInFrame: 1, knockedPins: 10 })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: ['frame must be a positive number'],
+        error: 'Bad Request',
+      });
+  });
+
+  it('/roll - with missing rollInFrame error', async () => {
+    await request(app.getHttpServer())
+      .post('/roll')
+      .send({ frame: 1, knockedPins: 10 })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: ['rollInFrame must be a positive number'],
+        error: 'Bad Request',
+      });
+  });
+
+  it('/roll - with missing knockedPins error', async () => {
+    await request(app.getHttpServer())
+      .post('/roll')
+      .send({ frame: 1, rollInFrame: 2 })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: [
+          'knockedPins must not be greater than 10',
+          'knockedPins must not be less than 0',
+        ],
+        error: 'Bad Request',
+      });
+  });
+
+  it('/roll - with error of duplicate roll', async () => {
+    await request(app.getHttpServer())
+      .post('/roll')
+      .send({ frame: 3, rollInFrame: 1, knockedPins: 3 })
+      .expect(201);
+
+    await request(app.getHttpServer())
+      .post('/roll')
+      .send({ frame: 3, rollInFrame: 1, knockedPins: 10 })
+      .expect(409);
   });
 });
