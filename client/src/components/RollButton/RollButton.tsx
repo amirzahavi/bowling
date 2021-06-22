33ba0cd41f780f92fa39718cf3595eef645fbdfa
amir_za <amirz@traxretail.com>
2021-06-22 14:39:00 +0300

    style(client): :lipstick: change submit button to be an icon

--- client/src/components/RollButton/RollButton.tsx
+++ client/src/components/RollButton/RollButton.tsx
@@ -6,5 +6,10 @@ import './RollButton.css';
 interface RollButtonProps {}
 
 export const RollButton: FC<RollButtonProps> = () => {
-  return <Button bordered>Roll!</Button>
+  return <Button bordered>
+    <svg className="submit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0V0z" fill="none"/>
+      <path d="M14 8.83L17.17 12 14 15.17V14H6v-4h8V8.83M12 4v4H4v8h8v4l8-8-8-8z"/>
+    </svg>
+  </Button>
 }
